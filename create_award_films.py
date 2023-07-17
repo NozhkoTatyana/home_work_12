85dde0cc98e4fad6b8f5a65be3a144b8ad7c7ec3
NozhkoTatyana <tatyana.nozhko@ukr.net>
2023-07-17 16:14:08 +0300

    first commit

---
+++ create_award_films.py
@@ -0,0 +1,25 @@
+from film_award import films_awards
+from pprintpp import pprint as pp
+
+
+awards_list = []
+title_f = ''
+t=[]
+for i in films_awards:
+    for j in i['results']:
+        title_f = j['movie']['title'].replace(' ', '_').replace(':', '')
+        if title_f:
+            awards_list.append({
+                                'type': j['type'],
+                                'award_name': j['award_name'],
+                                'award': j['award'],
+                                'title_film': title_f
+                })
+
+# pp(awards_list)
+
+awards = sorted(awards_list, key=lambda award: award['award_name'])
+#print(awards)
+
+
+
